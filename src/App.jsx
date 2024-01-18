58ca69fcf1c5e57a49b320a2f8ecdd41200a5780
Anton Ziuzikov <zuzikov.anton@gmail.com>
2024-01-18 15:45:05 +0300

    feat: implement new project input box

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import ProjectsSidebar from "./conponents/ProjectsSidebar.jsx";
 import NewProject from "./conponents/NewProject.jsx";
+import NoProjectSelected from "./conponents/NoProjectSelected.jsx";
+import {useState} from "react";
 
 function App() {
+
+    const [projectsState, setProjectsState] = useState({
+        selectedProject: undefined,
+        projects: []
+    });
+
+    function handleStartAddProject() {
+        setProjectsState(prevState => {
+            return {
+                ...prevState,
+                selectedProject: null,
+            };
+        });
+    }
+
+    let content;
+    if (projectsState.selectedProject === null) {
+        content = <NewProject/>
+    } else if (projectsState.selectedProject === undefined) {
+        content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+    }
+
   return (
     <>
         <main className="h-screen my-8 flex gap-8">
-            <ProjectsSidebar/>
-            <NewProject/>
+            <ProjectsSidebar onStartAddProject={handleStartAddProject}/>
+            {content}
         </main>
     </>
   );

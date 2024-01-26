1713f9371e211600988e24a47adfe2b90d347108
Anton Ziuzikov <zuzikov.anton@gmail.com>
2024-01-26 12:28:22 +0300

    feat: add modal window for invalid inputs

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import {useState} from "react";
 function App() {
 
     const [projectsState, setProjectsState] = useState({
-        selectedProject: undefined,
+        selectedProjectId: undefined,
         projects: []
     });
 
@@ -14,13 +14,14 @@ function App() {
         setProjectsState(prevState => {
             return {
                 ...prevState,
-                selectedProject: null,
+                selectedProjectId: null,
             };
         });
     }
 
     function handleAddProject(projectData) {
         setProjectsState(prevState => {
+            const projectId = Math.random();
             const newProject = {
                 ...projectData,
                 id: Math.random()
@@ -28,24 +29,23 @@ function App() {
 
             return {
                 ...prevState,
+                selectedProjectId: undefined,
                 projects: [...prevState.projects, newProject]
             }
         })
     }
 
-    console.log(projectsState);
-
     let content;
-    if (projectsState.selectedProject === null) {
+    if (projectsState.selectedProjectId === null) {
         content = <NewProject onAdd={handleAddProject}/>
-    } else if (projectsState.selectedProject === undefined) {
+    } else if (projectsState.selectedProjectId === undefined) {
         content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
     }
 
   return (
     <>
         <main className="h-screen my-8 flex gap-8">
-            <ProjectsSidebar onStartAddProject={handleStartAddProject}/>
+            <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
             {content}
         </main>
     </>
--- src/conponents/NewProject.jsx
+++ src/conponents/NewProject.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Input from "./Input.jsx";
 import {useRef} from "react";
+import Modal from "./Modal.jsx";
 
 function NewProject({onAdd}) {
+
+    const modal = useRef();
 
     const title = useRef();
     const description = useRef();
@@ -13,10 +16,24 @@ function NewProject({onAdd}) {
         const enteredDescription = description.current.value;
         const enteredDueDate = dueDate.current.value;
 
+        if(enteredTitle.trim() === ''
+            || enteredDescription.trim() === ''
+            || enteredDueDate.trim() ==='')
+        {
+            modal.current.open();
+            return;
+        }
+
         onAdd({title:enteredTitle, description: enteredDescription, dueDate: enteredDueDate});
     }
 
     return (
+        <>
+            <Modal ref={modal} buttonCaption="Close">
+                <h2 className="text-xl font-bold text-stone-500 my-4">Invalid Input</h2>
+                <p className="text-stone-400 mb-4">Oops... looks like you forgot to enter a value.</p>
+                <p className="text-stone-400 mb-4">Please make sure you provide a valid value for every input fields</p>
+            </Modal>
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
                 <li>
@@ -34,6 +51,7 @@ function NewProject({onAdd}) {
                 <Input type="date" ref={dueDate} label="Due Date"/>
             </div>
         </div>
+        </>
     );
 }
 
